e2ecb82a5890db4c52a90b00e1cd67d7be2b79c3
Sneha-Saurav <sneha.saurav@netsolutions.com>
2026-01-28 20:35:15 +0530

    removed value

--- Frontend/lib/utils/constants/staticValue.js
+++ Frontend/lib/utils/constants/staticValue.js
@@ -25,11 +25,11 @@ export const visitStatusOptions = [
 
 export const complaintOptions = [
   { label: "Hearing problem", value: "Hearing" },
-  { label: "Speech delay", value: "Speech" },
-  { label: "Cell/battery", value: "cell_battery" },
+  // { label: "Speech delay", value: "Speech" },
+  // { label: "Cell/battery", value: "cell_battery" },
   { label: "Machine cleaning", value: "machine_cleaning" },
   { label: "Follow-up", value: "follow_up" },
-  { label: "TGA", value: "tga" },
+  // { label: "TGA", value: "tga" },
   { label: "Service/Repair", value: "repair" },
   { label: "Hearing Testing", value: "hearing_test" },
   { label: "Other", value: "other" },
@@ -48,16 +48,16 @@ export const testRequestedOptions = [
 ];
 
 export const visitTypeOptions = [
-  { label: "New Test", value: "New Test" },
+  { label: "New Test Hearing", value: "New Test" },
   { label: "Follow-up", value: "Follow-up" },
   { label: "TGA / Machine Check", value: "TGA" },
-  { label: "Tip / Dome Change", value: "Tip / Dome Change" },
+  // { label: "Tip / Dome Change", value: "Tip / Dome Change" },
   // { label: "Machine cleaning", value: "machine_cleaning" },
-  { label: "Hearing Aid Trial", value: "Hearing Aid Trial" },
-  { label: "Hearing Aid Fitting", value: "Hearing Aid Fitting" },
-  { label: "Battery Purchase", value: "Battery Purchase" },
-  { label: "Speech Assessment", value: "Speech Assessment" },
-  { label: "Speech Therapy Follow-up", value: "Speech Therapy Follow-up" },
+  // { label: "Hearing Aid Trial", value: "Hearing Aid Trial" },
+  // { label: "Hearing Aid Fitting", value: "Hearing Aid Fitting" },
+  // { label: "Battery Purchase", value: "Battery Purchase" },
+  // { label: "Speech Assessment", value: "Speech Assessment" },
+  // { label: "Speech Therapy Follow-up", value: "Speech Therapy Follow-up" },
 ];
 
 export const previousHearingAidsOptions = [
